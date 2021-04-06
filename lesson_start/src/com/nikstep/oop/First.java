b0d95a9aaa056438310f6282d46503d55df91fa1
Oleksandr Nykytin <keeper@ninydev.com>
2021-04-06 15:45:11 +0300

    Array

--- lesson_start/src/com/nikstep/oop/First.java
+++ lesson_start/src/com/nikstep/oop/First.java
@@ -1,6 +1,25 @@
 package com.nikstep.oop;
 
 public class First {
+
+    private int [] arr;
+    int size;
+    public int[] insert (int toInsert, int pos){
+        int [] newArr = new int[size + 1];
+        for (int i =0; i < pos; i++) {
+            newArr[i] = arr[i];
+        }
+        newArr[pos] = toInsert;
+
+        for (int i = pos+1; i < size+2; i++) {
+            newArr[i] = arr[i-1];
+        }
+
+        arr = newArr;
+        size++;
+        return arr;
+    }
+
     // public, private, protected
     private First(){
         System.out.println("First is create");
